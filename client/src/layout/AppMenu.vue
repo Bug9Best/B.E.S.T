72c7206c18644d45175a1f4ff2f97845dcfa3054
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-05-20 17:51:29 +0700

    done menu

--- client/src/layout/AppMenu.vue
+++ client/src/layout/AppMenu.vue
@@ -1,82 +1,3 @@
-<script setup>
-import axios from 'axios'
-import { ref, watch, onMounted } from 'vue'
-import AppMenuItem from './AppMenuItem.vue'
-import io from 'socket.io-client'
-
-let user = {
-  id: 6
-}
-
-// const listEnroll = onMounted(async () => {
-//     try {
-//         const res = await axios.get('http://localhost:8080/api/enrollment/getEnroll', {
-//             params: {
-//                 id: user.id
-//             }
-//         })
-//         localStorage.setItem('myCourse', JSON.stringify(res.data));
-//     } catch (error) {
-//         console.log(error)
-//     }
-// });
-
-let myCourse = JSON.parse(localStorage.getItem('myCourse')) || []
-myCourse = myCourse.map((course) => {
-  return {
-    label: course.course.title,
-    icon: 'pi pi-fw pi-book',
-    to: '/course_detail/' + course.courseId
-  }
-})
-
-io('http://localhost:4000').on('enroll', () => {
-//   console.log('get course')
-//   JSON.parse(localStorage.getItem('myCourse')),
-//     (newVal) => {
-//       myCourse = newVal.map((course) => {
-//         return {
-//           label: course.title,
-//           icon: 'pi pi-fw pi-book',
-//           to: '/course_detail/' + course.courseId
-//         }
-//       })
-//     }
-
-
-    // getEnrollCourse()
-})
-
-const model = ref([
-  {
-    label: 'หน้า',
-    items: [
-      { label: 'คอร์สเรียน', icon: 'pi pi-fw pi-book', to: '/course' },
-      { label: 'ปฏิทิน', icon: 'pi pi-fw pi-calendar', to: '/calendar' },
-      { label: 'งานที่ได้รับมอบหมาย', icon: 'pi pi-fw pi-file', to: '/assignment' },
-      { label: 'แชท', icon: 'pi pi-fw pi-comments', to: '/chat' },
-      // { label: 'Setting', icon: 'pi pi-fw pi-cog', to: '/setting' },
-      // { label: 'User Manage', icon: 'pi pi-fw pi-user-edit', to: '/manage' },
-      {
-        label: 'คอร์สของฉัน',
-        icon: 'pi pi-fw pi-book',
-        to: '/course_detail',
-        items: myCourse
-      }
-    ]
-  }
-])
-
-const assistant = ref([
-  {
-    items: [
-      { label: 'เกี่ยวกับโปรแกรมนี้', icon: 'pi pi-fw pi-verified', to: '/about' },
-      { label: 'ทีมพัฒนา', icon: 'pi pi-fw pi-users', to: '/team' }
-    ]
-  }
-])
-</script>
-
 <template>
   <ul class="layout-menu flex flex-column flex-grow-0 flex-shrink-1">
     <template v-for="(item, i) in model" :key="item">
@@ -92,5 +13,77 @@ const assistant = ref([
     </template>
   </ul>
 </template>
+
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
+import AppMenuItem from './AppMenuItem.vue'
+import io from 'socket.io-client'
+
+export default {
+  components: {
+    AppMenuItem
+  },
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem('user')),
+      myCourse: [],
+      model: [
+        {
+          label: 'หน้า',
+          items: [
+            { label: 'คอร์สเรียน', icon: 'pi pi-fw pi-book', to: '/course' },
+            { label: 'ปฏิทิน', icon: 'pi pi-fw pi-calendar', to: '/calendar' },
+            { label: 'งานที่ได้รับมอบหมาย', icon: 'pi pi-fw pi-file', to: '/assignment' },
+            { label: 'แชท', icon: 'pi pi-fw pi-comments', to: '/chat' },
+            {
+              label: 'คอร์สของฉัน',
+              icon: 'pi pi-fw pi-book',
+              to: '/course_detail',
+              items: this.myCourse
+            }
+          ]
+        }
+      ],
+      assistant: [
+        {
+          items: [
+            { label: 'เกี่ยวกับโปรแกรมนี้', icon: 'pi pi-fw pi-verified', to: '/about' },
+            { label: 'ทีมพัฒนา', icon: 'pi pi-fw pi-users', to: '/team' }
+          ]
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.getEnrollments()
+    io('http://localhost:4000').on('enroll', () => {
+      this.getEnrollments()
+    })
+  },
+  methods: {
+    async getEnrollments() {
+      try {
+        const res = await axios.get('http://localhost:8080/api/enrollment/getEnroll', {
+          params: {
+            id: this.user.id
+          }
+        })
+        this.myCourse = res.data.map((course) => {
+          return {
+            label: course.course.title,
+            icon: 'pi pi-fw pi-book',
+            to: '/course_detail/' + course.courseId
+          }
+        })
+        this.model[0].items[4].items = this.myCourse
+        console.log(this.myCourse)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
 
 <style></style>
