95fc1c297b2e211a40b78689a8be83820f54aa05
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2023-05-21 14:08:02 +0700

    fixed top bar

--- client/src/layout/AppTopbar.vue
+++ client/src/layout/AppTopbar.vue
@@ -18,9 +18,8 @@ onBeforeUnmount(() => {
 })
 
 const logoUrl = computed(() => {
-  return `${contextPath}layout/images/${
-    layoutConfig.darkTheme.value ? 'logo-white' : 'logo-dark'
-  }.svg`
+  return `${contextPath}layout/images/${layoutConfig.darkTheme.value ? 'logo-white' : 'logo-dark'
+    }.svg`
 })
 
 const onTopBarMenuButton = () => {
@@ -72,22 +71,15 @@ export default {
       avatarLabel: '',
       items: [
         {
-          label: 'Actions',
+          label: 'หัวข้อ',
           items: [
             {
-              label: 'Profile',
-              icon: 'pi pi-user-edit',
-              command: () => {
-                this.$router.push('/profile')
-              }
-            },
-            {
-              label: 'Sign Out',
+              label: 'ลงชื่อออก',
               icon: 'pi pi-sign-out',
               command: () => {
                 localStorage.removeItem('token')
                 localStorage.removeItem('user')
-                this.$router.push('/')
+                this.$router.push('/signin')
               }
             }
           ]
@@ -97,72 +89,52 @@ export default {
   },
   created() {
     this.user = JSON.parse(localStorage.getItem('user'))
-    // console.log(this.user)
+    this.avatarLabel = this.user.fullname
+      .split(' ')
+      .map((n) => n[0])
+      .join('')
   },
   methods: {
     toggle(event) {
-      this.$refs.menu.toggle(event)
-    }
-    //     getLocalStorage() {
-    //         this.user = JSON.parse(localStorage.getItem('token'));
-    //         this.fullName = this.user.firstName + ' ' + this.user.lastName;
-    //         this.avatarLabel = this.user.firstName.charAt(0) + this.user.lastName.charAt(0);
-    //     }
+      this.$refs.menu.toggle(event);
+    },
   }
 }
 </script>
 
 <template>
-  <div class="layout-topbar">
-    <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
-      <i class="pi pi-bars"></i>
-    </button>
+  <div class="layout-topbar justify-content-between">
+    <div class="flex align-items-center">
+      <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
+        <i class="pi pi-bars"></i>
+      </button>
 
-    <router-link to="/course" class="layout-topbar-logo">
-      <!-- <img :src="logoUrl" alt="logo" /> -->
-      <span>B.E.S.T (Better Education Supported Technology)</span>
-    </router-link>
+      <router-link to="/course" class="layout-topbar-logo hidden md:block">
+        <!-- <img :src="logoUrl" alt="logo" /> -->
+        <span>B.E.S.T (Better Education Supported Technology)</span>
+      </router-link>
+    </div>
 
-    <button
-      class="p-link layout-topbar-menu-button layout-topbar-button"
-      @click="onTopBarMenuButton()"
-    >
+    <button class="p-link layout-topbar-menu-button layout-topbar-button" @click="onTopBarMenuButton()">
       <i class="pi pi-ellipsis-v"></i>
     </button>
 
-    <div class="layout-topbar-menu flex align-items-center" :class="topbarMenuClasses">
-      <div class="hidden md:block">
-        <router-link to="/notification">
-          <button class="p-link layout-topbar-button mr-2">
-            <i class="pi pi-bell"></i>
-            <span>Notification</span>
-          </button>
-        </router-link>
-
-        <Button
-          @click="toggle"
-          aria-haspopup="true"
-          aria-controls="overlay_menu"
-          class="p-button-text p-button-plain px-3 py-0"
-        >
-          <div class="flex align-items-center">
-            <p class="mt-3 mr-2 text-lg text-900">{{ this.user?.fullname }}</p>
-            <div
-              class="border-circle flex align-items-center justify-content-center"
-              style="
+    <div>
+      <Button @click="toggle" aria-haspopup="true" aria-controls="overlay_menu"
+        class="p-button-text p-button-plain px-3 py-0">
+        <div class="flex align-items-center">
+          <p class="mt-3 mr-2 text-lg text-900">{{ this.user?.fullname }}</p>
+          <div class="border-circle flex align-items-center justify-content-center" style="
                 background-color: var(--primary-color);
                 color: #ffffff;
                 width: 30px;
                 height: 30px;
-              "
-            >
-              {{ this.avatarLabel }}
-            </div>
+              ">
+            {{ this.avatarLabel }}
           </div>
-        </Button>
-
-        <Menu class="block md:hidden" id="overlay_menu" ref="menu" :model="items" :popup="true" />
-      </div>
+        </div>
+      </Button>
+      <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
     </div>
   </div>
 </template>
